21c5b13e4b29ed9aaf712333c5e43590eefce983
Chalie Choi <ddd1331@gmail.com>
2020-10-22 23:56:51 +0900

    define remove

--- code/src/com/allendowney/thinkdast/MyLinkedList.java
+++ code/src/com/allendowney/thinkdast/MyLinkedList.java
@@ -30,18 +30,20 @@ public class MyLinkedList<E> implements List<E> {
 			this.data = data;
 			this.next = null;
 		}
+
 		@SuppressWarnings("unused")
 		public Node(E data, Node next) {
 			this.data = data;
 			this.next = next;
 		}
+
 		public String toString() {
 			return "Node(" + data.toString() + ")";
 		}
 	}
 
-	private int size;            // keeps track of the number of elements
-	private Node head;           // reference to the first node
+	private int size; // keeps track of the number of elements
+	private Node head; // reference to the first node
 
 	/**
 	 *
@@ -73,7 +75,8 @@ public class MyLinkedList<E> implements List<E> {
 		} else {
 			Node node = head;
 			// loop until the last node
-			for ( ; node.next != null; node = node.next) {}
+			for (; node.next != null; node = node.next) {
+			}
 			node.next = new Node(element);
 		}
 		size++;
@@ -82,13 +85,13 @@ public class MyLinkedList<E> implements List<E> {
 
 	@Override
 	public void add(int index, E element) {
-		//TODO: FILL THIS IN!
+		// TODO: FILL THIS IN!
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends E> collection) {
 		boolean flag = true;
-		for (E element: collection) {
+		for (E element : collection) {
 			flag &= add(element);
 		}
 		return flag;
@@ -112,7 +115,7 @@ public class MyLinkedList<E> implements List<E> {
 
 	@Override
 	public boolean containsAll(Collection<?> collection) {
-		for (Object obj: collection) {
+		for (Object obj : collection) {
 			if (!contains(obj)) {
 				return false;
 			}
@@ -126,7 +129,9 @@ public class MyLinkedList<E> implements List<E> {
 		return node.data;
 	}
 
-	/** Returns the node at the given index.
+	/**
+	 * Returns the node at the given index.
+	 * 
 	 * @param index
 	 * @return
 	 */
@@ -135,7 +140,7 @@ public class MyLinkedList<E> implements List<E> {
 			throw new IndexOutOfBoundsException();
 		}
 		Node node = head;
-		for (int i=0; i<index; i++) {
+		for (int i = 0; i < index; i++) {
 			node = node.next;
 		}
 		return node;
@@ -143,16 +148,18 @@ public class MyLinkedList<E> implements List<E> {
 
 	@Override
 	public int indexOf(Object target) {
-		//TODO: FILL THIS IN!
+		// TODO: FILL THIS IN!
 		Node node = head;
-		for(int i=0; i<size; i++) {
-			if (equals(target, node.data)) return i;
+		for (int i = 0; i < size; i++) {
+			if (equals(target, node.data))
+				return i;
 			node = node.next;
 		}
 		return -1;
 	}
 
-	/** Checks whether an element of the array is the target.
+	/**
+	 * Checks whether an element of the array is the target.
 	 *
 	 * Handles the special case that the target is null.
 	 *
@@ -182,7 +189,7 @@ public class MyLinkedList<E> implements List<E> {
 	public int lastIndexOf(Object target) {
 		Node node = head;
 		int index = -1;
-		for (int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			if (equals(target, node.data)) {
 				index = i;
 			}
@@ -213,14 +220,21 @@ public class MyLinkedList<E> implements List<E> {
 
 	@Override
 	public E remove(int index) {
-		//TODO: FILL THIS IN!
-		return null;
+		E element = get(index);
+		if (index == 0) {
+			head = head.next;
+		} else {
+			Node node = getNode(index-1);
+			node.next = node.next.next;
+		}
+		size--;
+		return element;
 	}
 
 	@Override
 	public boolean removeAll(Collection<?> collection) {
 		boolean flag = true;
-		for (Object obj: collection) {
+		for (Object obj : collection) {
 			flag &= remove(obj);
 		}
 		return flag;
@@ -252,7 +266,7 @@ public class MyLinkedList<E> implements List<E> {
 		// TODO: classify this and improve it.
 		int i = 0;
 		MyLinkedList<E> list = new MyLinkedList<E>();
-		for (Node node=head; node != null; node = node.next) {
+		for (Node node = head; node != null; node = node.next) {
 			if (i >= fromIndex && i <= toIndex) {
 				list.add(node.data);
 			}
@@ -265,7 +279,7 @@ public class MyLinkedList<E> implements List<E> {
 	public Object[] toArray() {
 		Object[] array = new Object[size];
 		int i = 0;
-		for (Node node=head; node != null; node = node.next) {
+		for (Node node = head; node != null; node = node.next) {
 			// System.out.println(node);
 			array[i] = node.data;
 			i++;
